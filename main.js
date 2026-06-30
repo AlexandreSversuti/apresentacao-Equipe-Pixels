c887164d1a0eaf6db2206e5fb0b858874bed40d7
Aluno <alexandre.sversuti.filho@escola.pr.gov.br>
2026-06-30 09:37:46 -0300

    Estilização continuada e quase finalizada /  todos os arquivos foram alterados (30/06/26 - 11:47)

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 window.addEventListener("scroll", function() {
     const header = document.querySelector(".cabecalho");
-    const sessaoColab = document.querySelector(".principal__colab")
-    const posicaoSessaoColab = sessaoColab.getBoundingClientRect();
+    const sessaoColab = document.querySelector(".principal__colab");
+    const sessaoProjetos = document.querySelector(".principal__projetos");
 
-    if (posicaoSessaoColab.top <= 1 && posicaoSessaoColab.bottom >= 0) {
+    const posicaoSessaoColab = sessaoColab.getBoundingClientRect();
+    const posicaoSessaoProjetos = sessaoProjetos.getBoundingClientRect();
+
+    if (posicaoSessaoProjetos.top <= 50) {
+        header.style.backgroundColor = "#2e6e22";
+        header.style.boxShadow = "0rem 0.15rem 0.5rem #1d4616ab";
+        header.style.setProperty('--cor-hoverHeader', '#FFFFFF'); 
+        header.style.setProperty('--cor-background', '#2e6e22');
+    } else if (posicaoSessaoColab.top <= 50) {
         header.style.backgroundColor = "#633417";
         header.style.boxShadow = "0rem 0.15rem 0.5rem #4b2811";
         header.style.setProperty('--cor-hoverHeader', '#FFFFFF');
