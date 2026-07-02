a665d281dc8bb4ab64057d48a500550bea245ce1
Aluno <alexandre.sversuti.filho@escola.pr.gov.br>
2026-07-02 09:02:07 -0300

    Aba de projetos adicionado mais assets / problema da navegação resolvido (02/07/26 - 09:01)

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ window.addEventListener("scroll", function() {
     if (posicaoSessaoProjetos.top <= 50) {
         header.style.backgroundColor = "#2e6e22";
         header.style.boxShadow = "0rem 0.15rem 0.5rem #1d4616ab";
-        header.style.setProperty('--cor-hoverHeader', '#FFFFFF'); 
-        header.style.setProperty('--cor-background', '#2e6e22');
+        header.style.setProperty('--cor-hoverHeader', '#2e6e22'); 
+        header.style.setProperty('--cor-background', '#ffffff');
     } else if (posicaoSessaoColab.top <= 50) {
         header.style.backgroundColor = "#633417";
         header.style.boxShadow = "0rem 0.15rem 0.5rem #4b2811";
@@ -20,6 +20,6 @@ window.addEventListener("scroll", function() {
         header.style.backgroundColor = "#5CD6DB";
         header.style.boxShadow = "0rem 0.15rem 0.5rem rgba(118, 170, 165, 0.865)";
         header.style.setProperty('--cor-hoverHeader', '#5CD6DB');
-        header.style.setProperty('--cor-background', '#FFFFFF');
+        header.style.setProperty('--cor-background', '#ffffff');
     }
 });
