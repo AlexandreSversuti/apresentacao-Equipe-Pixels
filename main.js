7a1d4a975e6dc8d4455ba3a506199d94e9d4b0fd
Alexandre Sversuti <alexandre.sversuti.filho@escola.pr.gov.br>
2026-09-15 20:58:14 -0300

    feat: adiciona sessão inicial de feedback

--- main.js
+++ main.js
@@ -23,3 +23,32 @@ window.addEventListener("scroll", function() {
         header.style.setProperty('--cor-background', '#ffffff');
     }
 });
+
+//Envio de dados do feedback para o servidor e tirar sessão do formulário do site kkk
+const formulario = document.querySelector('#formulario');
+const inputNome = document.querySelector('#nome');
+const inputEmail = document.querySelector('#email');
+const inputFeedback = document.querySelector('#feedback');
+const agradecimento = document.querySelector('#agradecimento')
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (inputNome.checkValidity() && inputEmail.checkValidity() && inputFeedback.checkValidity()) {
+        const nome = inputNome.value;
+        const email = inputEmail.value;
+        const feedback = inputFeedback.value;
+
+        const dados = {
+            nome,
+            email,
+            feedback
+        }
+
+        console.log(dados);
+        }
+
+        formulario.setAttribute('hidden', 'true');
+        agradecimento.removeAttribute('hidden');
+    
+});
